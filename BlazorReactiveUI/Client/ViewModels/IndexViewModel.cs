403ea4bc3e7c035e4941aec493b1af7b1ef23435
Ryan Melena <ryan.melena@noesislabs.com>
2020-03-31 13:02:13 -0500

    Initial Commit

---
+++ BlazorReactiveUI/Client/ViewModels/IndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorReactiveUI.Client.ViewModels
+{
+    public class IndexViewModel : ReactiveObject
+    {
+        private readonly ObservableAsPropertyHelper<long> _foo;
+
+        public IndexViewModel()
+        {
+            Observable.Interval(new TimeSpan(0, 0, 1))
+                .ToProperty(this, x => x.Foo, out _foo, 0);
+        }
+
+        public long Foo => _foo.Value;
+    }
+}
---
+++ BlazorReactiveUI/Client/Views/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using BlazorReactiveUI.Client.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorReactiveUI.Client.Views
+{
+    public partial class Index
+    {
+        public Index()
+        {
+            ViewModel = new IndexViewModel();
+        }
+    }
+}
